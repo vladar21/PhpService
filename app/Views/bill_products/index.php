f0e22bbd41deec2535a68ec2cbcba9759b92ba29
vladar21 <vladar21@gmail.com>
2023-08-13 02:08:14 +0300

    fix: fix datatable on products view

--- app/Views/bill_products/index.php
+++ app/Views/bill_products/index.php
@@ -20,31 +20,34 @@ echo generate_breadcrumbs($breadcrumbs, ' / ');
 <!-- Button to load products -->
 <button type="submit" id="loadProductsBtn" class="btn btn-primary float-right">Update Products</button>
 <div class="clear-both"></div>
-<table id="billProductsTable">
-    <thead>
-    <tr>
-        <th><?= lang('app_lang.id') ?></th>
-        <th><?= lang('app_lang.name') ?></th>
-        <th><?= lang('app_lang.description') ?></th>
-        <th><?= lang('app_lang.price_net') ?></th>
-        <th><?= lang('app_lang.quantity') ?></th>
-        <th><?= lang('app_lang.quantity_unit') ?></th>
-        <th><?= lang('app_lang.additional_info') ?></th>
-        <th><?= lang('app_lang.price_gross') ?></th>
-        <th><?= lang('app_lang.form_name') ?></th>
-        <th><?= lang('app_lang.code') ?></th>
-        <th><?= lang('app_lang.currency') ?></th>
-        <th><?= lang('app_lang.weight_unit') ?></th>
-        <th><?= lang('app_lang.supplier_code') ?></th>
-        <th><?= lang('app_lang.created_at') ?></th>
-        <th><?= lang('app_lang.updated_at') ?></th>
-        <th><?= lang('app_lang.actions') ?></th>
-    </tr>
-    </thead>
-    <tbody>
+<div class="table-container">
+    <table id="billProductsTable" class="dataTable">
+        <thead>
+        <tr>
+            <th><?= lang('app_lang.id') ?></th>
+            <th><?= lang('app_lang.name') ?></th>
+            <th><?= lang('app_lang.description') ?></th>
+            <th><?= lang('app_lang.price_net') ?></th>
+            <th><?= lang('app_lang.quantity') ?></th>
+            <th><?= lang('app_lang.quantity_unit') ?></th>
+            <th><?= lang('app_lang.additional_info') ?></th>
+            <th><?= lang('app_lang.price_gross') ?></th>
+            <th><?= lang('app_lang.form_name') ?></th>
+            <th><?= lang('app_lang.code') ?></th>
+            <th><?= lang('app_lang.currency') ?></th>
+            <th><?= lang('app_lang.weight_unit') ?></th>
+            <th><?= lang('app_lang.supplier_code') ?></th>
+            <th><?= lang('app_lang.created_at') ?></th>
+            <th><?= lang('app_lang.updated_at') ?></th>
+            <th><?= lang('app_lang.actions') ?></th>
+        </tr>
+        </thead>
+        <tbody>
 
-    </tbody>
-</table>
+        </tbody>
+    </table>
+    <br/>
+</div>
 
 <link rel="stylesheet" href="<?= base_url('assets/css/bill_products/index.css') ?>">
 <script src="<?= base_url('assets/js/bill_products/index.js') ?>"></script>
