811b6b32d5f5d6ac4132aa70e2cf70104d4ae2b1
vladar21 <vladar21@gmail.com>
2023-09-09 18:04:23 +0300

    fix: remove DSN

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -73,7 +73,7 @@ class Database extends Config
     public function __construct()
     {
         // В конструкторе устанавливаем значения переменных класса
-        $this->default['DSN'] = getenv('DATABASE_URL') ? 'dbname=df48n3jmhiipp0 host=ec2-54-195-120-0.eu-west-1.compute.amazonaws.com port=5432 user=ezfyiejucncezp password=c65d8c4acce60609b48afed78b2e9257636d9395a1c8bd08a8089a44d99197ef sslmode=require' : '';
+//        $this->default['DSN'] = getenv('DATABASE_URL') ? 'dbname=df48n3jmhiipp0 host=ec2-54-195-120-0.eu-west-1.compute.amazonaws.com port=5432 user=ezfyiejucncezp password=c65d8c4acce60609b48afed78b2e9257636d9395a1c8bd08a8089a44d99197ef sslmode=require' : '';
         $this->default['hostname'] = getenv('DATABASE_URL') ? '' : '127.0.0.1';
         $this->default['username'] = getenv('DATABASE_URL') ? '': 'root';
         $this->default['password'] = getenv('DATABASE_URL') ? '' : 'root';
