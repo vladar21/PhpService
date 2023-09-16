d11e80cb090f38a94c7f4a05d0d5dae41f344def
vladar21 <vladar21@gmail.com>
2023-09-16 23:35:42 +0300

    docs: add docstring commits to MeestSenderRecipientModel

--- app/Models/MeestSenderRecipientModel.php
+++ app/Models/MeestSenderRecipientModel.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
+/**
+ * Model class for managing meest user's data.
+ */
 class MeestSenderRecipientModel extends Model
 {
+    // Database configuration
     protected $table = 'meest_senders_recipients';
-
     protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
 
     protected $allowedFields = [
         'id',
@@ -28,10 +32,16 @@ class MeestSenderRecipientModel extends Model
         'zipCode',
     ];
 
-    protected $useAutoIncrement = true;
-
+    // Dates configuration
     protected $useTimestamps = true;
 
+    /**
+     * Get sender or recipient clients based on the provided ID, or retrieve all clients if ID is false.
+     *
+     * @param int|false $id The client ID to retrieve or false to retrieve all clients.
+     *
+     * @return array|null An array of client data or null if not found.
+     */
     public function getClients($id = false)
     {
         if ($id === false)
@@ -40,6 +50,13 @@ class MeestSenderRecipientModel extends Model
             return $this->asArray()->where(['id' => $id])->first();
     }
 
+    /**
+     * Get a client by their Bill Client ID.
+     *
+     * @param int $bill_client_id The Bill Client ID to retrieve the client.
+     *
+     * @return array|null An array of client data or null if not found.
+     */
     public function getClientByBillClientId($bill_client_id){
         return $this->asArray()->where(['bill_client_id' => $bill_client_id])->first();
     }
