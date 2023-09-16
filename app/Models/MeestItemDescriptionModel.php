4ca58d447ed61b5f7c8d91b9992d136817314584
vladar21 <vladar21@gmail.com>
2023-09-16 23:07:39 +0300

    docs: add docstring commits to MeestItemDescriptionModel

--- app/Models/MeestItemDescriptionModel.php
+++ app/Models/MeestItemDescriptionModel.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
+/**
+ * Model class for managing meest item description data.
+ */
 class MeestItemDescriptionModel extends Model
 {
+    // Database configuration
     protected $table = 'meest_item_descriptions';
-
     protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
 
+    // Allowed fields in the database
     protected $allowedFields = [
         'id',
         'item_id',
@@ -17,15 +22,26 @@ class MeestItemDescriptionModel extends Model
         'lang',
     ];
 
-    protected $useAutoIncrement = true;
-
+    // Dates configuration
     protected $useTimestamps = true;
 
+    /**
+     * Define a many-to-one relationship with the MeestItem model.
+     *
+     * @return \CodeIgniter\Database\BaseBuilder
+     */
     public function item()
     {
         return $this->belongsTo('App\Models\MeestItem', 'item_id');
     }
 
+    /**
+     * Get item descriptions based on the provided item_id.
+     *
+     * @param int|null $item_id The ID of the item to retrieve descriptions for.
+     *
+     * @return array|null An array of item descriptions or null if not found.
+     */
     public function getItemDescription($item_id = null){
         if ($item_id){
             return $this->asArray()->where(['item_id' => $item_id])->findAll();
