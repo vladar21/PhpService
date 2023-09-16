1359c525387b6395e915930abb36408062758243
vladar21 <vladar21@gmail.com>
2023-09-16 22:45:24 +0300

    docs: add docstring commits to BillPositionModel

--- app/Models/BillPositionModel.php
+++ app/Models/BillPositionModel.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
+/**
+ * Model class for managing bill positions data.
+ */
 class BillPositionModel extends Model
 {
+    // Database configuration
     protected $DBGroup          = 'default';
     protected $table            = 'bill_positions';
     protected $primaryKey       = 'id';
@@ -13,22 +17,24 @@ class BillPositionModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
+
+    // Allowed fields in the database
     protected $allowedFields = ['id', 'invoice_id', 'name', 'description', 'price_net', 'quantity', 'total_price_gross', 'total_price_net', 'account_id', 'created_at', 'updated_at', 'additional_info', 'quantity_unit', 'tax', 'price_gross', 'price_tax', 'total_price_tax', 'kind', 'invoice_position_id', 'product_id', 'deleted', 'discount', 'discount_percent', 'tax2', 'exchange_rate', 'accounting_tax_kind', 'code', 'discount_net', 'lump_sum_tax', 'corrected_pos_kind', 'gtu_code'];
 
-    // Dates
+    // Dates configuration
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
-    // Validation
+    // Validation rules and messages (if any)
     protected $validationRules      = [];
     protected $validationMessages   = [];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
-    // Callbacks
+    // Callbacks configuration
     protected $allowCallbacks = true;
     protected $beforeInsert   = [];
     protected $afterInsert    = [];
@@ -39,12 +45,26 @@ class BillPositionModel extends Model
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
 
+    /**
+     * Get positions for a specific invoice.
+     *
+     * @param int|false $invoice_id The invoice's ID to retrieve positions for, or false to retrieve all positions.
+     *
+     * @return array|null The positions data.
+     */
     public function getPositionsInvoice($invoice_id = false)
     {
         if ($invoice_id)
             return $this->asArray()->where(['invoice_id' => $invoice_id])->findAll();
     }
 
+    /**
+     * Get a list of positions from the database.
+     *
+     * @param int|false $id The position's ID to retrieve, or false to retrieve all positions.
+     *
+     * @return array|object The position data.
+     */
     public function getPositions($id = false)
     {
         if ($id === false)
