32a291feb4eb0677de99dcacd8572103b193f3d9
vladar21 <vladar21@gmail.com>
2023-09-16 23:32:37 +0300

    docs: add docstring commits to MeestSenderRecipientDocumentModel

--- app/Models/MeestSenderRecipientDocumentModel.php
+++ app/Models/MeestSenderRecipientDocumentModel.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
+/**
+ * Model class for managing meest user's docs data.
+ */
 class MeestSenderRecipientDocumentModel extends Model
 {
+    // Database configuration
     protected $table = 'meest_sender_recipient_documents';
-
     protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
 
+    // Allowed fields in the database
     protected $allowedFields = [
         'id',
         'sender_recipient_id',
@@ -21,10 +26,16 @@ class MeestSenderRecipientDocumentModel extends Model
         'series',
     ];
 
-    protected $useAutoIncrement = true;
-
+    // Dates configuration
     protected $useTimestamps = true;
 
+    /**
+     * Get documents based on the provided sender or recipient ID, or retrieve all documents if ID is false.
+     *
+     * @param int|false $id The sender or recipient ID to retrieve documents for, or false to retrieve all documents.
+     *
+     * @return array|null An array of documents or null if not found.
+     */
     public function getDocs($id = false)
     {
         if ($id === false)
@@ -33,6 +44,13 @@ class MeestSenderRecipientDocumentModel extends Model
             return $this->asArray()->where(['sender_recipient_id' => $id])->first();
     }
 
+    /**
+     * Get all documents associated with a specific sender or recipient based on the provided ID.
+     *
+     * @param int $id The sender or recipient ID to retrieve documents for.
+     *
+     * @return array An array of documents associated with the sender or recipient.
+     */
     public function getAllClinetDocs($id)
     {
         return $this->asArray()->where(['sender_recipient_id' => $id])->findAll();
